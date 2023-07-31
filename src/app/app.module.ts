53a54ba53f6b7419797a3eb2b42902c1c1fad276
Gabriel <ggarcia7@utmachala.edu.ec>
2023-07-31 10:03:14 -0500

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegistrosComponent } from './components/registros/registros.component';
+import { RegistroComponent } from './components/registro/registro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistrosComponent,
+    RegistroComponent
+
   ],
   imports: [
     BrowserModule,
